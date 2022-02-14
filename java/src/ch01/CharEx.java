a086e9d7fe20d9ada6ac1bf4bdd69aca2b4883b1
vmffnxm9218 <vmffnxm9218@hanmail.net>
2022-02-14 11:12:57 +0900

    java first commit

---
+++ java/src/ch01/CharEx.java
@@ -0,0 +1,11 @@
+package ch01;
+
+public class CharEx {
+	public static void main(String[] args) {
+		char ch = 'A';
+		System.out.println(ch);
+		System.out.println((int)ch);
+		
+		
+	}
+}
---
+++ java/src/ch01/DoubleEx.java
@@ -0,0 +1,12 @@
+package ch01;
+
+public class DoubleEx {
+	public static void main(String[] args) {
+		double a = 10.5;
+		System.out.println(a);
+		float b = 10.5F;
+		System.out.println(b);
+//		int c = 10.5;
+//		System.out.println(c);
+	}
+}
